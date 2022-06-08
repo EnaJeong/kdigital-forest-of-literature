8b002d8ff8e02847eab18afc2d061fece06a35bf
EnaJeong <gekritzel1007@gmail.com>
2022-06-08 23:08:30 +0900

    Update book code scraping to scrap all categories at once

--- analysis/scraping_01_book_code_bs_ver.py
+++ analysis/scraping_01_book_code_bs_ver.py
@@ -7,7 +7,6 @@ import pandas as pd
 import requests
 
 
-NATION = "spain"
 DIRECTORY = "./analysis/data"
 
 CATEGORIES = {
@@ -23,13 +22,14 @@ CATEGORIES = {
     "others": "017001045015",
 }
 
-national_code = CATEGORIES[NATION]
-file_name = f"{DIRECTORY}/book_code_{NATION}.csv"
+
+def save_file(df: pd.DataFrame, category: str):
+    file_name = f"{DIRECTORY}/book_code_{category}.csv"
+    df.to_csv(file_name)
 
 
-##############################################################################
-def get_url(page=1):
-    return f"http://www.yes24.com/24/Category/Display/{national_code}?ParamSortTp=03&AO=2&PageNumber={page}"
+def get_url(category_code: str, page: int = 1):
+    return f"http://www.yes24.com/24/Category/Display/{category_code}?ParamSortTp=03&AO=2&PageNumber={page}"
 
 
 # CSS selector
@@ -47,34 +47,36 @@ PAT_END_PAGE = r"(?<=PageNumber=).+$"
 
 # 체험판 제외
 REMOVAL_TYPE = "체험판"
-##############################################################################
 
-url = get_url()
-soup = BeautifulSoup(requests.get(url).text, "html.parser")
-
-end_btn = soup.select(CSS_END_BUTTON)[0]
-end_page = re.search(PAT_END_PAGE, end_btn.get(ATTR_END_PAGE)).group()
-
-titles = {}
-
-for page in range(1, int(end_page) + 1):
-    url = get_url(page)
-    soup = BeautifulSoup(requests.get(url).text, "html.parser")
-    books = soup.select(CSS_BOOK_TITLE)
-
-    for book in books:
-        if REMOVAL_TYPE in book.text:
-            continue
-
-        code = PAT_BOOK_CODE.findall(book.get(ATTR_BOOK_CODE))[0]
-        titles[code] = book.text
-
-# 수집한 code와 title 정보 csv로 저장
-df_title = pd.DataFrame(titles.items(), columns=["code", "title"])
-df_title.set_index("code", inplace=True)
 
 if not os.path.exists(DIRECTORY):
     os.mkdir(DIRECTORY)
-df_title.to_csv(file_name)
 
-print(df_title)
+for category, category_code in CATEGORIES.items():
+    print(category, category_code)
+
+    url = get_url(category_code)
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+
+    end_btn = soup.select(CSS_END_BUTTON)[0]
+    end_page = re.search(PAT_END_PAGE, end_btn.get(ATTR_END_PAGE)).group()
+
+    titles = {}
+
+    for page in range(1, int(end_page) + 1):
+        url = get_url(category_code, page)
+        soup = BeautifulSoup(requests.get(url).text, "html.parser")
+        books = soup.select(CSS_BOOK_TITLE)
+
+        for book in books:
+            if REMOVAL_TYPE in book.text:
+                continue
+
+            code = PAT_BOOK_CODE.findall(book.get(ATTR_BOOK_CODE))[0]
+            titles[code] = book.text
+
+    df_title = pd.DataFrame(titles.items(), columns=["code", "title"])
+    df_title.set_index("code", inplace=True)
+    save_file(df_title, category)
+
+    print(df_title)
