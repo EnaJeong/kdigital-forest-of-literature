62d52e374f94e4fa1b41cff8ede0d5fa182001b7
HyeonJoo Jeong <30485707+EnaJeong@users.noreply.github.com>
2022-06-09 23:08:04 +0900

    Clean up modeling (#10)

--- analysis/modeling_01_tfidf.py
+++ analysis/modeling_01_tfidf.py
@@ -1,15 +1,22 @@
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from scipy.io import mmwrite, mmread
 import pickle
 
-df_book = pd.read_csv('./datasets/book_keyword.csv', index_col=0)
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.io import mmwrite
+
+
+FILE_INPUT = "./analysis/datasets/book_token.csv"
+FILE_MATRIX = "./analysis/datasets/tfidf.mtx"
+FILE_PICKLE = "./analysis/datasets/tfidf.pickle"
+
+
+df_book = pd.read_csv(FILE_INPUT, index_col=0)
 
 # tf -> 문장 안의 단어 수 / df -> 모든 문서에서 단어 수 (idx -> inverse dx)
 Tfidf = TfidfVectorizer(sublinear_tf=True)
-Tfidf_matrix = Tfidf.fit_transform(df_book['info'])
+Tfidf_matrix = Tfidf.fit_transform(df_book["info"])
 
-mmwrite('./datasets/tfidf.mtx', Tfidf_matrix)
+mmwrite(FILE_MATRIX, Tfidf_matrix)
 
-with open("./datasets/tfidf.pickle", "wb") as f:
+with open(FILE_PICKLE, "wb") as f:
     pickle.dump(Tfidf, f)
